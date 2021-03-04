912065b13ff72eb3f314e8b26cf9a4d693f83e07
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-04 13:16:53 +0300

    Render BooksList and BooksForm components

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import BooksForm from '../container/BooksForm';
+import BooksList from '../container/BooksList';
+
+function App() {
+  return (
+    <div className="App">
+      <BooksList />
+      <BooksForm />
+    </div>
+  );
+}
+
+export default App;
