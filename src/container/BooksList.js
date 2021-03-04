e5c438eb9fe0588b42b9a4b46b1327b4956f5dbc
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-04 17:56:44 +0300

    Use mapStateToProps inplace of useSelector hook

--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -1,29 +1,35 @@
-import { useSelector } from 'react-redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Book from '../components/Book';
 
-const BooksList = () => {
-  const books = useSelector(state => state.books);
-  return (
-    <>
-      <h2>List of Books</h2>
-      <table className="books-list">
-        <thead>
-          <tr>
-            <th>ID.</th>
-            <th>Title</th>
-            <th>Category</th>
+const BooksList = ({ books }) => (
+  <>
+    <h2>List of Books</h2>
+    <table className="books-list">
+      <thead>
+        <tr>
+          <th>ID.</th>
+          <th>Title</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        { books && books.map(book => (
+          <tr key={Math.random()}>
+            <Book bookObj={book} />
           </tr>
-        </thead>
-        <tbody>
-          { books.map(book => (
-            <tr key={Math.random()}>
-              <Book bookObj={book} />
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+        ))}
+      </tbody>
+    </table>
+  </>
+);
+
+BooksList.propTypes = {
+  books: PropTypes.instanceOf(Array).isRequired,
 };
 
-export default BooksList;
+const mapStateToProps = state => ({
+  books: state.books,
+});
+
+export default connect(mapStateToProps)(BooksList);
