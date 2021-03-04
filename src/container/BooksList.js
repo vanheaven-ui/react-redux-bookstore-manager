b552fd4609f30758ada7702340c125b471672450
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-04 13:19:01 +0300

    Implement the BooksList component

---
+++ src/container/BooksList.js
@@ -0,0 +1,28 @@
+import { useSelector } from 'react-redux';
+import Book from '../components/Book';
+
+const BooksList = () => {
+  const books = useSelector(state => state.books);
+  return (
+    <>
+      <table className="books-list">
+        <thead>
+          <tr>
+            <th>ID.</th>
+            <th>Title</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          { books.map(book => (
+            <tr key={Math.random()}>
+              <Book bookObj={book} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default BooksList;
