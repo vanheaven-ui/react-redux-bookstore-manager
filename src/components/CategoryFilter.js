ce4f3497afc1b2d3344c5456876122c69f5fbdd9
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-05 17:31:46 +0300

    Correct typo

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -2,8 +2,8 @@ import PropTypes from 'prop-types';
 import { bookCategories } from '../container/BooksForm';
 
 const CategoryFilter = ({ handleChange }) => (
-  <select defaultValue="ALL" onChange={handleChange}>
-    <option value="ALL">ALL</option>
+  <select defaultValue="All" onChange={handleChange}>
+    <option value="All">All</option>
     { bookCategories && bookCategories.map(cat => (
       <option value={cat} key={cat}>{cat}</option>
     ))}
