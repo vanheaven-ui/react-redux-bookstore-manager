152dc099e2110f323c647ac03fc89991c37050eb
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-05 11:19:16 +0300

    Implement handleRemove click handler

--- src/container/BooksList.js
+++ src/container/BooksList.js
@@ -1,36 +1,49 @@
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
+import { REMOVE_BOOK } from '../actions';
 
-const BooksList = ({ books }) => (
-  <>
-    <h2>List of Books</h2>
-    <table className="books-list">
-      <thead>
-        <tr>
-          <th>ID.</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        { books && books.map(book => (
-          <tr key={Math.random()}>
-            <Book bookObj={book} />
-            <td><button type="button">Remove</button></td>
+const BooksList = ({ books, removeBook }) => {
+  const handleRemove = book => {
+    removeBook(book);
+  };
+
+  return (
+    <>
+      <h2>List of Books</h2>
+      <table className="books-list">
+        <thead>
+          <tr>
+            <th>ID.</th>
+            <th>Title</th>
+            <th>Category</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
-  </>
-);
+        </thead>
+        <tbody>
+          { books && books.map(book => (
+            <tr key={Math.random()}>
+              <Book bookObj={book} removeBook={() => handleRemove(book)} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 BooksList.propTypes = {
   books: PropTypes.instanceOf(Array).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = state => ({
   books: state.books,
 });
 
-export default connect(mapStateToProps)(BooksList);
+const mapDispatchToProps = dispatch => (
+  {
+    removeBook: book => dispatch(REMOVE_BOOK(book)),
+  }
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
