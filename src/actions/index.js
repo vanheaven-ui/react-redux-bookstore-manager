48bd94a047334d239de6a2456fa7a769aac364d5
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-05 15:20:26 +0300

    Add CHANGE_FILTER action

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,4 +8,9 @@ const REMOVE_BOOK = book => ({
   book,
 });
 
-export { CREATE_BOOK, REMOVE_BOOK };
+const CHANGE_FILTER = criteria => ({
+  type: 'CHANGE_FILTER',
+  criteria,
+});
+
+export { CREATE_BOOK, REMOVE_BOOK, CHANGE_FILTER };
