c14c20e6427e2575fa75fb5c6fef375977cc1884
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-05 13:00:05 +0300

    Adjust remove book buttin as per specifications

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,7 +5,7 @@ const Book = ({ bookObj, removeBook }) => (
     <td>{`${bookObj.id}.`}</td>
     <td>{bookObj.title}</td>
     <td>{bookObj.category}</td>
-    <td><button type="button" onClick={removeBook}>Remove</button></td>
+    <td><button type="button" onClick={removeBook}>Remove Book</button></td>
   </>
 );
 
