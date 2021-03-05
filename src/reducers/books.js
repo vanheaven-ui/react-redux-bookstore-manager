f8e2f3a1cce5585869abdda8606e3c9983288118
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-06 00:59:34 +0300

    Remove debugging line

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,7 +1,6 @@
 const booksReducer = (state = [], action) => {
   switch (action.type) {
     case 'REMOVE_BOOK':
-      console.log(state.filter(book => book.id !== action.book.id));
       return state.filter(book => book.id !== action.book.id);
     case 'CREATE_BOOK':
       return [...state, action.book];
