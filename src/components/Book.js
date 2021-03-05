2872ec3d9b5b93c82678d33eeb659db7b1271f6c
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-05 11:16:59 +0300

    Add remove book button and receive removeBook prop

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
 
-const Book = ({ bookObj }) => (
+const Book = ({ bookObj, removeBook }) => (
   <>
     <td>{`${bookObj.id}.`}</td>
     <td>{bookObj.title}</td>
     <td>{bookObj.category}</td>
+    <td><button type="button" onClick={removeBook}>Remove</button></td>
   </>
 );
 
 Book.propTypes = {
   bookObj: PropTypes.instanceOf(Object).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default Book;
