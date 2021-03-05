e2b37ca04ec5f19718a99fc491c5761a316d8506
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-05 15:39:17 +0300

    Add bookCategories as options in CategoryFilter component

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,6 +1,12 @@
+import { bookCategories } from "../container/BooksForm";
+
 const CategoryFilter = () => (
-  <select>
-    <option value="default">ALl</option>
+  <select defaultValue="default">
+    <option value="default">ALL</option>
+    { bookCategories && bookCategories.map(cat => (
+      <option value={cat} key={cat}>{cat}</option>
+    ))}
+    <option value="default">All</option>
   </select>
 );
 
