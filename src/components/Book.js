372fc6692dc95742604ef7145030f21d9da81178
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-06 22:53:04 +0300

    Update jsx element rendered by Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,10 +2,30 @@ import PropTypes from 'prop-types';
 
 const Book = ({ bookObj, removeBook }) => (
   <>
-    <td>{`${bookObj.id}.`}</td>
-    <td>{bookObj.title}</td>
-    <td>{bookObj.category}</td>
-    <td><button type="button" onClick={removeBook}>Remove Book</button></td>
+    <div className="book-details">
+      <h5 className="category">{bookObj.category}</h5>
+      <h2>{bookObj.title}</h2>
+      <a href="/author" className="author">Author</a>
+      <ul className="extra">
+        <li><a href="/comments">Comments</a></li>
+        <li><span className="line-1" /></li>
+        <button type="button" onClick={removeBook}>Remove</button>
+        <li><span className="line-1" /></li>
+        <li><a href="/edit">Edit</a></li>
+      </ul>
+    </div>
+    <div className="progress-stats">
+      <span className="oval" />
+      <div className="text-stats">
+        <p className="percent">64%</p>
+        <p className="progress-status">Completed</p>
+      </div>
+    </div>
+    <div className="progress-update">
+      <p className="curr-chapter">CURRENT CHAPTER</p>
+      <p className="Chapter-num">Chapter 17</p>
+      <button type="button">UPDATE PROGRESS</button>
+    </div>
   </>
 );
 
