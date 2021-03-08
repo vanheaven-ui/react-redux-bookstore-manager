ba00640018c1f66754ac9cdccbecac86eebe125b
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-03-09 00:02:41 +0300

    Remove debugging line

--- src/components/CategoryButton.js
+++ src/components/CategoryButton.js
@@ -1,11 +1,8 @@
 import PropTypes from 'prop-types';
 
-const CategoryButton = ({ category, handleChange }) => {
-  console.log(category);
-  return (
-    <button type="button" onClick={handleChange}>{category}</button>
-  );
-};
+const CategoryButton = ({ category, handleChange }) => (
+  <button type="button" onClick={handleChange}>{category}</button>
+);
 
 CategoryButton.propTypes = {
   category: PropTypes.string.isRequired,
